c65295ecb530d4ef05631db94e9f938122dfe563
Gnutyud <nguyenduytungtd3@gmail.com>
2021-04-08 22:43:24 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -15,26 +15,34 @@ function App() {
   }, []);
   // fetch Notes data from server
   const fetchNotes = async () => {
-    const res = await fetch('http://localhost:5000/ourNotes');
+    const res = await fetch(
+      'https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes',
+    );
     const data = await res.json();
     return data;
   };
   // Delete func
   const delNote = async (id) => {
-    await fetch(`http://localhost:5000/ourNotes/${id}`, { method: 'DELETE' });
+    await fetch(
+      `https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes/${id}`,
+      { method: 'DELETE' },
+    );
     setNote(ourNotes.filter((note) => note.id !== id));
   };
   // Set value to expansion Add note form
   const [showAddNote, setshowAddNote] = useState(false);
   // Save notes value from input
   const saveNote = async (note) => {
-    const res = await fetch('http://localhost:5000/ourNotes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const res = await fetch(
+      'https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
       },
-      body: JSON.stringify(note),
-    });
+    );
     const data = await res.json();
     setNote([data, ...ourNotes]);
     // const id = Math.floor(Math.random() * 1000) + 1;
@@ -43,18 +51,22 @@ function App() {
   };
   // Edit func
   const editNote = async (updateNote) => {
-    const res = await fetch(`http://localhost:5000/ourNotes/${updateNote.id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
+    const res = await fetch(
+      `https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes/${updateNote.id}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: updateNote.title,
+          text: updateNote.text,
+        }),
       },
-      body: { title: updateNote.title, text: updateNote.text },
-    });
+    );
     const data = await res.json();
     console.log(data);
-    // setNote(
-    //   ourNotes.map((note) => (note.id === updateNote.id ? updateNote : note)),
-    // );
+    setNote(ourNotes.map((note) => (note.id === updateNote.id ? data : note)));
   };
 
   return (
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,30 +1,29 @@
-import { useState } from "react";
-import NoteForm from "./NoteForm";
+import { useState } from 'react';
+import NoteForm from './NoteForm';
 
 const Add = ({ openAdd, closeAdd, display, onSave }) => {
-  const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
   // set number of row in textarea to fit with content input
-  const [numLine, setNumLine] = useState("1");
-  let textarea = document.getElementById("text-area");
+  const [numLine, setNumLine] = useState('1');
+  let textarea = document.getElementById('text-area');
   if (textarea) {
-    textarea.addEventListener("keyup", () => {
-      var num = textarea.value.split("\n");
+    textarea.addEventListener('keyup', () => {
+      var num = textarea.value.split('\n');
       setNumLine(num.length);
-      console.log(num.length);
     });
   }
   // when click Close button
   const onSubmit = (e) => {
     e.preventDefault();
-    setNumLine("1");
+    setNumLine('1');
     if (!text && !title) {
       closeAdd();
       return;
     }
     onSave({ title, text });
-    setTitle("");
-    setText("");
+    setTitle('');
+    setText('');
     closeAdd();
   };
 
@@ -37,8 +36,8 @@ const Add = ({ openAdd, closeAdd, display, onSave }) => {
       setText={setText}
       title={title}
       setTitle={setTitle}
-      cName={"add-content"}
-      submitName={"Close"}
+      cName={'add-content'}
+      submitName={'Close'}
       numLine={numLine}
     />
   );
