87cf78a589d5b58c907e64c54cb9ae1aafa61c28
Gnutyud <nguyenduytungtd3@gmail.com>
2021-04-08 21:30:05 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,53 +1,60 @@
-import Add from "./components/Add";
-import Header from "./components/Header";
-import Notes from "./components/Notes";
-import React, { useState } from "react";
+import Add from './components/Add';
+import Header from './components/Header';
+import Notes from './components/Notes';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [ourNotes, setNote] = useState([
-    {
-      id: 1,
-      title: "",
-      text: "Study",
-      day: "March 30, 2021",
-      reminder: false,
-    },
-    {
-      id: 2,
-      title: "Exercise",
-      text: "running 30m everyday",
-      day: "",
-      reminder: true,
-    },
-    {
-      id: 3,
-      title: "Web dev",
-      text: "Get a job",
-      day: "someday in May 2021",
-      reminder: true,
-    },
-  ]);
+  const [ourNotes, setNote] = useState([]);
+  // Call api
+  useEffect(() => {
+    const getNotes = async () => {
+      const noteFromServer = await fetchNotes();
+      setNote(noteFromServer);
+    };
+    getNotes();
+  }, []);
+  // fetch Notes data from server
+  const fetchNotes = async () => {
+    const res = await fetch('http://localhost:5000/ourNotes');
+    const data = await res.json();
+    return data;
+  };
   // Delete func
-  const delNote = (id) => {
+  const delNote = async (id) => {
+    await fetch(`http://localhost:5000/ourNotes/${id}`, { method: 'DELETE' });
     setNote(ourNotes.filter((note) => note.id !== id));
   };
   // Set value to expansion Add note form
   const [showAddNote, setshowAddNote] = useState(false);
   // Save notes value from input
-  const saveNote = (note) => {
-    const id = Math.floor(Math.random() * 1000) + 1;
-    const newNote = { id, ...note };
-    setNote([newNote, ...ourNotes]);
+  const saveNote = async (note) => {
+    const res = await fetch('http://localhost:5000/ourNotes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(note),
+    });
+    const data = await res.json();
+    setNote([data, ...ourNotes]);
+    // const id = Math.floor(Math.random() * 1000) + 1;
+    // const newNote = { id, ...note };
+    // setNote([newNote, ...ourNotes]);
   };
   // Edit func
-  const editNote = (updateId, updateTitle, updateText) => {
-    setNote(
-      ourNotes.map((note) =>
-        note.id === updateId
-          ? { id: updateId, title: updateTitle, text: updateText }
-          : note,
-      ),
-    );
+  const editNote = async (updateNote) => {
+    const res = await fetch(`http://localhost:5000/ourNotes/${updateNote.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: { title: updateNote.title, text: updateNote.text },
+    });
+    const data = await res.json();
+    console.log(data);
+    // setNote(
+    //   ourNotes.map((note) => (note.id === updateNote.id ? updateNote : note)),
+    // );
   };
 
   return (
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import EditNote from "./EditNote";
+import React, { useState } from 'react';
+import EditNote from './EditNote';
 
 const Notes = ({ notes, onDelete, editNote }) => {
   const [id, setId] = useState(null);
-  const [text, setText] = useState("");
-  const [title, setTitle] = useState("");
+  const [text, setText] = useState('');
+  const [title, setTitle] = useState('');
   const submitUpdate = () => {
-    editNote(id, title, text);
+    editNote({ id, title, text });
     setId(null);
-    setTitle("");
-    setText("");
+    setTitle('');
+    setText('');
   };
   if (id) {
     return (
-      <div id="myModal" className="modal">
+      <div id='myModal' className='modal'>
         <EditNote
           onSubmit={submitUpdate}
           text={text}
@@ -26,23 +26,23 @@ const Notes = ({ notes, onDelete, editNote }) => {
   }
 
   return (
-    <div className="note-content">
+    <div className='note-content'>
       {notes.map((note) => (
-        <div key={note.id} className="note-box">
+        <div key={note.id} className='note-box'>
           <i
-            className="fa fa-pencil"
-            aria-hidden="true"
+            className='fa fa-pencil'
+            aria-hidden='true'
             onClick={() => {
               setId(note.id);
               setTitle(note.title);
               setText(note.text);
             }}></i>
           <i
-            className="fa fa-trash-o"
-            aria-hidden="true"
+            className='fa fa-trash-o'
+            aria-hidden='true'
             onClick={() => onDelete(note.id)}></i>
-          <i class="fa fa-paint-brush"></i>
-          <h3>{(note.title = "" ? "No title" : note.title)}</h3>
+          <i class='fa fa-paint-brush'></i>
+          <h3>{(note.title = '' ? 'No title' : note.title)}</h3>
           <p>{note.text}</p>
         </div>
       ))}
