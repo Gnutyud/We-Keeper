781b2e62cc6dd22f3340034cbecd95abe3aadaf2
Gnutyud <nguyenduytungtd3@gmail.com>
2021-04-09 22:21:41 +0700

    change server address

--- src/App.js
+++ src/App.js
@@ -15,34 +15,28 @@ function App() {
   }, []);
   // fetch Notes data from server
   const fetchNotes = async () => {
-    const res = await fetch(
-      'https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes',
-    );
+    const res = await fetch('https://gnutyud-server.herokuapp.com/notes');
     const data = await res.json();
     return data;
   };
   // Delete func
   const delNote = async (id) => {
-    await fetch(
-      `https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes/${id}`,
-      { method: 'DELETE' },
-    );
+    await fetch(`https://gnutyud-server.herokuapp.com/notes/${id}`, {
+      method: 'DELETE',
+    });
     setNote(ourNotes.filter((note) => note.id !== id));
   };
   // Set value to expansion Add note form
   const [showAddNote, setshowAddNote] = useState(false);
   // Save notes value from input
   const saveNote = async (note) => {
-    const res = await fetch(
-      'https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(note),
+    const res = await fetch('https://gnutyud-server.herokuapp.com/notes/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify(note),
+    });
     const data = await res.json();
     setNote([data, ...ourNotes]);
     // const id = Math.floor(Math.random() * 1000) + 1;
@@ -51,19 +45,16 @@ function App() {
   };
   // Edit func
   const editNote = async (updateNote) => {
-    const res = await fetch(
-      `https://my-json-server.typicode.com/gnutyud/we-keeper/ourNotes/${updateNote.id}`,
-      {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title: updateNote.title,
-          text: updateNote.text,
-        }),
+    const res = await fetch(`n/${updateNote.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
       },
-    );
+      body: JSON.stringify({
+        title: updateNote.title,
+        text: updateNote.text,
+      }),
+    });
     const data = await res.json();
     setNote(ourNotes.map((note) => (note.id === updateNote.id ? data : note)));
   };
