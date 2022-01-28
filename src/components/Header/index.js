5e95c9c0a3340e49de6b2c2cea76cbce597075e9
Gnutyud <nguyenduytungtd3@gmail.com>
2022-01-28 17:18:02 +0700

    response UI

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,10 @@
 import logo from '../../assets/images/logo.jpg';
 import PropTypes from 'prop-types';
 import Search from './Search';
-// import { useState } from 'react';
 
 const Header = ({
   title,
-  onClick,
-  // isSearch,
-  // searchClose,
-  // searchText,
-  // setSearchText,
 }) => {
-  // const [searchText, setSearchText] = useState("");
-  
-  // const onChange = () => {
-  //   onClick(searchText);
-  // };
 
   return (
     <div className='header-nav'>
@@ -24,13 +13,8 @@ const Header = ({
         <p>{title}</p>
       </div>
       <Search
-        // onClick={onChange}
-        text='search what you want...'
+        text='search here...'
         cname='btn btn-color search'
-        // search={searchText}
-        // setSearch={setSearchText}
-        // isSearch={isSearch}
-        // searchClose={searchClose}
       />
     </div>
   );
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+import reactDom from "react-dom";
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClick}></div>;
+};
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {reactDom.createPortal(
+        <BackDrop onClick={props.onClick} />,
+        document.getElementById("overlays"),
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        document.getElementById("overlays"),
+      )}
+    </React.Fragment>
+  );
+};
+export default Modal;
