72849b66f78ec43b367b2d53167333f953d3bbe8
Gnutyud <nguyenduytungtd3@gmail.com>
2022-10-19 22:28:22 +0700

    add auth middleware for all note routes

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const noteController = require('../controllers/noteController');
+const { authByToken } = require('../middleware/auth');
 
 router.route('/')
-  .get(noteController.getAllNotes)
-  .post(noteController.createNewNote)
-  .patch(noteController.updateNote)
-  .delete(noteController.deleteNote)
+  .get(authByToken, noteController.getAllNotes)
+  .post(authByToken, noteController.createNewNote)
+  .patch(authByToken, noteController.updateNote)
+  .delete(authByToken, noteController.deleteNote)
 
   module.exports = router;
